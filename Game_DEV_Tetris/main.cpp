579fb85d9b3ba9d0159e1786ce6bb36b7e6326bd
TermiJP <javier.corchado.fer@gmail.com>
2026-09-09 16:21:14 +0200

    figuras

--- Game_DEV_Tetris/main.cpp
+++ Game_DEV_Tetris/main.cpp
@@ -1,5 +1,20 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <time.h>
+
+const int M = 20;
+const int N = 10;
+
+int figures[7][4] =
+{
+    1,3,5,7, // I
+    2,4,5,7, // Z
+    3,5,4,6, // S
+    3,5,4,7, // T
+    2,3,5,7, // L
+    3,5,7,6, // J
+    2,3,4,5, // O
+};
 
 int main()
 {
@@ -10,6 +25,7 @@ int main()
     t.loadFromFile("images/tiles.png");
 
     sf::Sprite sprite(t);
+    sprite.setTextureRect(sf::IntRect({ 0, 0 }, { 18, 18 }));
 
     // Start the game loop
     while (window.isOpen())
